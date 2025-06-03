52977c13f0609749f9f3a95800a714dd8935f89e
zoegodin-admin <zhodin9@gmail.com>
2025-06-03 08:52:40 -0400

    Task-04 added possibility to change the names of the team in the home page

--- info-feud/src/app/pages/home/home.component.ts
+++ info-feud/src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../../components/button-component/button-component.component';
+import { TeamService } from '../../services/team.service';
 
 @Component({
   selector: 'home-page',
@@ -8,4 +9,11 @@ import { ButtonComponent } from '../../components/button-component/button-compon
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
+  constructor(public teamService: TeamService) {}
+
+
+  changeTeamName(team: 'team1' | 'team2', event: any){
+    this.teamService.setTeamName(team, event.target.value)
+  }
 }
