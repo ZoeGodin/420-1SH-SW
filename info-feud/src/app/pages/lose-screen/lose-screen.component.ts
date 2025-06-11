e34ce9e1b6ecb0958d33cb673e9083bd38a058e4
zoegodin-admin <zhodin9@gmail.com>
2025-06-11 11:42:10 -0400

    Added losing screen

--- info-feud/src/app/pages/lose-screen/lose-screen.component.ts
+++ info-feud/src/app/pages/lose-screen/lose-screen.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../../components/button-component/button-component.component';
 
 @Component({
   selector: 'app-lose-screen',
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './lose-screen.component.html',
   styleUrl: './lose-screen.component.css'
 })
 export class LoseScreenComponent {
 
+  ngAfterViewInit(): void {
+    this.playSadNoise();
+  }
+
+  playSadNoise(): void {
+    let audio = new Audio;
+    audio.src = "assets/wah-wah.mp3";
+    audio.load();
+    audio.play();
+  }
 }
